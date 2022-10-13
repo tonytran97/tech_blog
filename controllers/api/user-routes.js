fc7ca4338acea92a669477c3008f2d6a67fa0805
tonytran97 <tonytran_97@live.com>
2022-10-12 23:19:16 -0700

    fixed 404 error, eventlistener works. login issue

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,44 +3,34 @@ const { User } = require('../../models');
 
 // login
 router.post('/login', async (req, res) => {
-    try {
-        const dbUserData = await User.findOne({
-          where: {
-            username: req.body.username,
-          },
-        });
-console.log(dbUserData);
-        if (!dbUserData) {
-            res
-              .status(400)
-              .json({ message: 'Incorrect username or password. Please try again!' });
-            return;
-        }
+  try {
+    const userData = await User.findOne({ where: { username: req.body.username } });
+    console.log(req.body.username);
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password, please try again' });
+      return;
+    }
 
-        const validPassword = await dbUserData.checkPassword(req.body.password);
-
+    const validPassword = await userData.checkPassword(req.body.password);
+console.log(req.body.password);
     if (!validPassword) {
       res
         .status(400)
-        .json({ message: 'Incorrect username or password. Please try again!' });
+        .json({ message: 'Incorrect username or password, please try again' });
       return;
     }
 
     req.session.save(() => {
-        req.session.loggedIn = true;
-        console.log(
-          '🚀 ~ file: user-routes.js ~ line 57 ~ req.session.save ~ req.session.cookie',
-          req.session.cookie
-        );
-  
-        res
-          .status(200)
-          .json({ user: dbUserData, message: 'You are now logged in!' });
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }  
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -3,22 +3,21 @@ const loginFormHandler = async (event) => {
   
     const username = document.querySelector('#username-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
-  console.log(username);
+console.log(username);
     if (username && password) {
       const response = await fetch('/api/users/login', {
         method: 'POST',
         body: JSON.stringify({ username, password }),
         headers: { 'Content-Type': 'application/json' },
       });
-  
+console.log(response);
       if (response.ok) {
         document.location.replace('/');
         alert('You have been logged in');
-        console.log('You have been logged in');
       } else {
         alert('Failed to log in.');
       }
     }
   };
   
-  document.querySelector('.login-form').addEventListener('submit', console.log('this is working'));
+  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
